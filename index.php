482d71644292fff0e27b52dffdcb0a157b83ec4f
oYi52 <twbbstsuna@gmail.com>
2022-01-25 14:56:49 +0800

    作品收藏

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+
+$_con = mysqli_connect("localhost","example","kYcM1XuFebgqftpm","example");
+$sql ="SELECT * FROM `ex_works`";
+$query = mysqli_query($_con, $sql);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,21 +12,68 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
 </head>
 <body>
-    <h1>Hello World!</h1>
-    <hr>
-    <form action="process.php" method="POST">
-        <input type="text" name="i" value="<?php if(isset($_GET['i'])){echo $_GET['i'];} ?>">
-        <select name="count" id="count">
-            <option value="1" <?php if(isset($_GET['count'])&&$_GET['count']=="1"){echo "selected";} ?>>+</option>
-            <option value="2" <?php if(isset($_GET['count'])&&$_GET['count']=="2"){echo "selected";} ?>>-</option>
-            <option value="3" <?php if(isset($_GET['count'])&&$_GET['count']=="3"){echo "selected";} ?>>*</option>
-            <option value="4" <?php if(isset($_GET['count'])&&$_GET['count']=="4"){echo "selected";} ?>>/</option>
-        </select>
-        <input type="text" name="j" value="<?php if(isset($_GET['j'])){echo $_GET['j'];} ?>">
-        <input type="submit" value="=">
-        <?php if(isset($_GET['result'])){echo $_GET['result'];} ?>
-    </form>
+
+<div class="container">
+    <?php if(isset($_GET['result'])&&$_GET['result']=="success"){echo "<h3>新增成功!</h3>";} ?>
+    <div class="card mt-3 p-3">
+        <h2>作品列表</h2>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>作品分類</th>
+                    <th>作品名稱</th>
+                    <th>年份</th>
+                    <th>材質</th>
+                    <th>尺寸</th>
+                    <th>收藏狀態</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while($row=mysqli_fetch_assoc($query)){ ?>
+                <tr>
+                    <td><?php echo $row['wocategory']; ?></td>
+                    <td><?php echo $row['wotitle']; ?></td>
+                    <td><?php echo $row['woyear']; ?></td>
+                    <td><?php echo $row['womaterial']; ?></td>
+                    <td><?php echo $row['wosize']; ?></td>
+                    <td><?php echo $row['wostatus']; ?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="card mt-3 p-3">
+        <h2>新增作品</h2>
+        <hr>
+        <form action="process.php" method="POST">
+            <label for="">作品分類</label>
+            <select name="wocategory" id="wocategory" class="form-control">
+                <option value="分類一">分類一</option>
+                <option value="分類二">分類二</option>
+                <option value="分類三">分類三</option>
+            </select>
+            <label>作品名稱</label>
+            <input type="text" name="wotitle" id="wotitle" class="form-control">
+            <label>年份</label>
+            <input type="text" name="woyear" id="woyear" class="form-control">
+            <label>材質</label>
+            <input type="text" name="womaterial" id="womaterial" class="form-control">
+            <label>尺寸</label>
+            <input type="text" name="wosize" id="wosize" class="form-control">
+            <label>收藏狀態</label>
+            <input type="text" name="wostatus" id="wostatus" class="form-control">
+            <label>作品圖片</label>
+            <input type="file" name="wofile" id="wofile" class="form-control">
+            <hr>
+            <input type="submit" class="form-control btn-success" value="新增作品">
+        </form>
+    </div>
+</div>
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </html>
--- process.php
+++ process.php
@@ -1,4 +1,21 @@
-<?php 
+<?php
+
+/*
+echo $_POST['wocategory'];
+echo $_POST['wotitle'];
+echo $_POST['woyear'];
+echo $_POST['womaterial'];
+echo $_POST['wosize'];
+echo $_POST['wostatus'];
+echo $_POST['wofile'];
+*/
+$_con = mysqli_connect("localhost","example","kYcM1XuFebgqftpm","example");
+echo $sql ="INSERT INTO `ex_works` (`woid`, `wocategory`, `wotitle`, `woyear`, `womaterial`, `wosize`, `wostatus`, `woext`) VALUES (NULL, '".$_POST['wocategory']."', '".$_POST['wotitle']."', '".$_POST['woyear']."', '".$_POST['womaterial']."', '".$_POST['wosize']."', '".$_POST['wostatus']."', '".$_POST['wofile']."');";
+$query = mysqli_query($_con, $sql);
+header("Location: index.php?result=success");
+exit;
+
+/*
     //判斷輸入值是否為數字
     if(!(is_numeric($_POST['i'])&&is_numeric($_POST['j']))){
         echo "輸入值非數字！";
@@ -37,4 +54,5 @@
     //將運算過程與結果傳送回原來的頁面
     header("Location: index.php?i=".$_POST['i']."&j=".$_POST['j']."&count=".$_POST['count']."&result=".$result);
     exit;
+*/
 ?>
