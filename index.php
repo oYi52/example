16e802856c8e15975338812e43450eefcedb9f9f
WenHaoYi <twbbstsuna@gmail.com>
2022-02-15 15:45:58 +0800

    20220215更新：新增刪除與編輯功能，還有回家作業在裡面

--- index.php
+++ index.php
@@ -17,12 +17,21 @@ $query = mysqli_query($_con, $sql);
 </head>
 <body>
 
-<div class="container">
+<div class="container mb-3">
     <?php if(isset($_GET['result'])&&$_GET['result']=="success"){echo "<h3>新增成功!</h3>";} ?>
     <?php if(isset($_GET['result'])&&$_GET['result']=="failed"){echo "<h3>新增失敗!</h3>";} ?>
     <?php if(isset($_GET['result'])&&$_GET['result']=="nofile"){echo "<h3>未選擇檔案!</h3>";} ?>
     <div class="card mt-3 p-3">
-        <h2>作品列表</h2>
+        <div class="row">
+            <div class="col-4">
+                <h2>作品列表</h2>
+            </div>
+            <div class="col-2 offset-6">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal">
+                新增作品
+            </button>
+            </div>
+        </div>
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -32,6 +41,7 @@ $query = mysqli_query($_con, $sql);
                     <th>材質</th>
                     <th>尺寸</th>
                     <th>收藏狀態</th>
+                    <th>操作</th>
                 </tr>
             </thead>
             <tbody>
@@ -43,37 +53,36 @@ $query = mysqli_query($_con, $sql);
                     <td><?php echo $row['womaterial']; ?></td>
                     <td><?php echo $row['wosize']; ?></td>
                     <td><?php echo $row['wostatus']; ?></td>
+                    <td>
+                        <a href="index.php?edit=<?php echo $row['woid']; ?>" class="btn btn-sm btn-success">編輯</a>
+                        <form action="process.php" method="POST">
+                            <input type="hidden" name="woid" value="<?php echo $row['woid']; ?>">
+                            <input type="submit" name="action" class="btn btn-sm btn-danger" value="刪除" onclick="return confirm('確認要刪除嗎？')">
+                        </form>
+                    </td>
                 </tr>
                 <?php } ?>
             </tbody>
         </table>
     </div>
-    <div class="card mt-3 p-3">
-        <h2>新增作品</h2>
-        <hr>
-        <form action="process.php" method="POST" enctype="multipart/form-data">
-            <label for="">作品分類</label>
-            <select name="wocategory" id="wocategory" class="form-control">
-                <option value="分類一">分類一</option>
-                <option value="分類二">分類二</option>
-                <option value="分類三">分類三</option>
-            </select>
-            <label>作品名稱</label>
-            <input type="text" name="wotitle" id="wotitle" class="form-control" required>
-            <label>年份</label>
-            <input type="text" name="woyear" id="woyear" class="form-control"  required>
-            <label>材質</label>
-            <input type="text" name="womaterial" id="womaterial" class="form-control"  required>
-            <label>尺寸</label>
-            <input type="text" name="wosize" id="wosize" class="form-control"  required>
-            <label>收藏狀態</label>
-            <input type="text" name="wostatus" id="wostatus" class="form-control"  required>
-            <label>作品圖片</label>
-            <input type="file" name="wofile" id="wofile" class="form-control"  required>
-            <hr>
-            <input type="submit" name="action" class="form-control btn-success" value="新增作品"  required>
-        </form>
+    <?php if(isset($_GET['add'])){ ?>
+    <div class="modal fade" id="viewModal" tabindex="-1" aria-labelledby="viewModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="viewModalLabel"><?php echo $row['wotitle']; ?></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img src="uploadfiles/<?php echo $row['wocategory']."/".$row['woid'].".".$row['woext']; ?>" class="img-fluid" alt="">
+            </div>
+            <div class="modal-footer">
+                <a href="uploadfiles/<?php echo $row['wocategory']."/".$row['woid'].".".$row['woext']; ?>" class="btn btn-primary" download="<?php echo $row['wotitle'].".".$row['woext']; ?>">下載</a>
+            </div>
+            </div>
+        </div>
     </div>
+    <?php } ?>
     <?php if(isset($_GET['id'])){ 
         
         $sql ="SELECT * FROM `ex_works` WHERE `woid` = '".$_GET['id']."'";
@@ -92,9 +101,89 @@ $query = mysqli_query($_con, $sql);
                 <img src="uploadfiles/<?php echo $row['wocategory']."/".$row['woid'].".".$row['woext']; ?>" class="img-fluid" alt="">
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
                 <a href="uploadfiles/<?php echo $row['wocategory']."/".$row['woid'].".".$row['woext']; ?>" class="btn btn-primary" download="<?php echo $row['wotitle'].".".$row['woext']; ?>">下載</a>
             </div>
+            </div>
+        </div>
+    </div>
+    <?php } ?>
+    <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addModalLabel">新增作品</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+                <form action="process.php" method="POST" enctype="multipart/form-data">
+                    <div class="modal-body">
+                            <label for="">作品分類</label>
+                            <select name="wocategory" id="wocategory" class="form-control">
+                                <option value="分類一">分類一</option>
+                                <option value="分類二">分類二</option>
+                                <option value="分類三">分類三</option>
+                            </select>
+                            <label>作品名稱</label>
+                            <input type="text" name="wotitle" id="wotitle" class="form-control" required>
+                            <label>年份</label>
+                            <input type="text" name="woyear" id="woyear" class="form-control"  required>
+                            <label>材質</label>
+                            <input type="text" name="womaterial" id="womaterial" class="form-control"  required>
+                            <label>尺寸</label>
+                            <input type="text" name="wosize" id="wosize" class="form-control"  required>
+                            <label>收藏狀態</label>
+                            <input type="text" name="wostatus" id="wostatus" class="form-control"  required>
+                            <label>作品圖片</label>
+                            <input type="file" name="wofile" id="wofile" class="form-control"  required>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="submit" name="action" class="form-control btn-success" value="新增作品"  required>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <?php if(isset($_GET['edit'])){ 
+        
+        $sql ="SELECT * FROM `ex_works` WHERE `woid` = '".$_GET['edit']."'";
+        $query = mysqli_query($_con, $sql);
+        $row = mysqli_fetch_assoc($query);
+    
+    ?>
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addModalLabel">編輯「<?php echo $row['wotitle']; ?>」作品</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+                <form action="process.php" method="POST" enctype="multipart/form-data">
+                    <div class="modal-body">
+                            <label for="">作品分類</label>
+                            <select name="wocategory" id="wocategory" class="form-control">
+                                <option value="分類一" <?php if($row['wocategory']=="分類一"){echo "selected";} ?>>分類一</option>
+                                <option value="分類二" <?php if($row['wocategory']=="分類二"){echo "selected";} ?>>分類二</option>
+                                <option value="分類三" <?php if($row['wocategory']=="分類三"){echo "selected";} ?>>分類三</option>
+                            </select>
+                            <input type="hidden" name="wocatorigin" value="<?php echo $row['wocategory']; ?>">
+                            <label>作品名稱</label>
+                            <input type="text" name="wotitle" id="wotitle" class="form-control" value="<?php echo $row['wotitle']; ?>" required>
+                            <label>年份</label>
+                            <input type="text" name="woyear" id="woyear" class="form-control" value="<?php echo $row['woyear']; ?>"  required>
+                            <label>材質</label>
+                            <input type="text" name="womaterial" id="womaterial" class="form-control"  value="<?php echo $row['womaterial']; ?>" required>
+                            <label>尺寸</label>
+                            <input type="text" name="wosize" id="wosize" class="form-control" value="<?php echo $row['wosize']; ?>"  required>
+                            <label>收藏狀態</label>
+                            <input type="text" name="wostatus" id="wostatus" class="form-control" value="<?php echo $row['wostatus']; ?>"  required>
+                            <label>作品圖片</label>
+                            <img src="uploadfiles/<?php echo $row['wocategory']."/".$row['woid'].".".$row['woext'] ?>" class="img-fluid img-thumbnail">
+                            <input type="file" name="wofile" id="wofile" class="form-control">
+                            <input type="hidden" name="woid" value="<?php echo $row['woid']; ?>">
+                    </div>
+                    <div class="modal-footer">
+                        <input type="submit" name="action" class="form-control btn-success" value="編輯作品"  required>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -104,11 +193,17 @@ $query = mysqli_query($_con, $sql);
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 <script>
+    <?php if(isset($_GET['id'])){ ?>
     var myModal = new bootstrap.Modal(document.getElementById('viewModal'), {
         keyboard: false
     });
-    <?php if(isset($_GET['id'])){ ?>
     myModal.show();
+    <?php } ?>
+    <?php if(isset($_GET['edit'])){ ?>
+    var editModal = new bootstrap.Modal(document.getElementById('editModal'), {
+        keyboard: false
+    });
+    editModal.show();
     <?php } ?>
 </script>
 </html>
--- process.php
+++ process.php
@@ -1,36 +1,36 @@
 <?php
 
-/*
-echo $_POST['wocategory'];
-echo $_POST['wotitle'];
-echo $_POST['woyear'];
-echo $_POST['womaterial'];
-echo $_POST['wosize'];
-echo $_POST['wostatus'];
-echo $_POST['wofile'];
-*/
 $_con = mysqli_connect("localhost","example","kYcM1XuFebgqftpm","example");
 
 if(isset($_POST['action'])&&$_POST['action']=="新增作品"){
     if(isset($_FILES['wofile'])){
         //定義上傳路徑
-        $upload_dir="uploadfiles/".$_POST['wocategory']."/";
+        echo $upload_dir="uploadfiles/".$_POST['wocategory']."/";
+
+        //確認路徑是否存在
         if(!is_dir($upload_dir)){
-            if(!mkdir($upload_dir)){
+            //如果不存在，就新增並給予權限
+            if(!mkdir($upload_dir,0755)){
                 header("Location: index.php?result=mkdirfailed");
                 exit;
             }
         }
-        
+        //宣告允許上傳的附檔名陣列
         $allow_ext=array("jpg","png","pdf","tif");
-        echo $filename=$_FILES['wofile']['name'];
-        echo $ext=strtolower(pathinfo($_FILES['wofile']['name'], PATHINFO_EXTENSION));
-
+        //檔案上傳的檔名
+        $filename=$_FILES['wofile']['name'];
+        //透過檔名取得附檔名資訊
+        $ext=strtolower(pathinfo($_FILES['wofile']['name'], PATHINFO_EXTENSION));
+        
+        //檢查是否為可上傳之副檔名
         if(in_array($ext,$allow_ext)){
+            //插入一筆記錄，這裡會先插入一筆記錄是因為等等要用到這筆記錄的ID
             echo $sql ="INSERT INTO `ex_works` (`woid`, `wocategory`, `wotitle`, `woyear`, `womaterial`, `wosize`, `wostatus`, `woext`) VALUES (NULL, '".$_POST['wocategory']."', '".$_POST['wotitle']."', '".$_POST['woyear']."', '".$_POST['womaterial']."', '".$_POST['wosize']."', '".$_POST['wostatus']."', '".$ext."');";
             $query = mysqli_query($_con, $sql);
             if($query){
+                //將上傳目的地、剛剛插入的紀錄ID跟附檔名合成成新的檔名
                 $new_file_name=$upload_dir.mysqli_insert_id($_con).".".$ext;
+                //搬移檔案至目的地，並確認有沒有完成
                 if(move_uploaded_file($_FILES['wofile']['tmp_name'],$new_file_name)){
                     header("Location: index.php?result=success");
                     exit;
@@ -53,44 +53,69 @@ if(isset($_POST['action'])&&$_POST['action']=="新增作品"){
     }
 }
 
-/*
-    //判斷輸入值是否為數字
-    if(!(is_numeric($_POST['i'])&&is_numeric($_POST['j']))){
-        echo "輸入值非數字！";
+
+if(isset($_POST['action'])&&$_POST['action']=="刪除"){
+    echo $sql ="DELETE FROM `ex_works` WHERE `woid` = '".$_POST['woid']."'";
+    $query = mysqli_query($_con, $sql);
+    header("Location: index.php?result=deleted");
+    exit;
+}
+
+
+if(isset($_POST['action'])&&$_POST['action']=="編輯作品"){
+
+    //先編輯文本
+    $sql = "UPDATE `ex_works` SET `wocategory` = '".$_POST['wocategory']."', `wotitle` = '".$_POST['wotitle']."', `woyear` = '".$_POST['woyear']."', `womaterial` = '".$_POST['womaterial']."', `wosize` = '".$_POST['wosize']."', `wostatus` = '".$_POST['wostatus']."' WHERE `woid` = ".$_POST['woid'];
+    $query = mysqli_query($_con, $sql);
+
+    //判斷是否有改類別
+    /* 這次的回家作業寫在這裡 */
+
+    //再判斷是不是有重新上傳檔案
+    if(isset($_FILES['wofile'])){
+        //定義路徑
+        echo $upload_dir="uploadfiles/".$_POST['wocategory']."/";
+
+        //確認路徑是否存在
+        if(!is_dir($upload_dir)){
+            //如果不存在，就新增並給予權限
+            if(!mkdir($upload_dir,0755)){
+                header("Location: index.php?result=mkdirfailed");
+                exit;
+            }
+        }
+        //宣告允許上傳的附檔名陣列
+        $allow_ext=array("jpg","png","pdf","tif");
+        //檔案上傳的檔名
+        $filename=$_FILES['wofile']['name'];
+        //透過檔名取得附檔名資訊
+        $ext=strtolower(pathinfo($_FILES['wofile']['name'], PATHINFO_EXTENSION));
+        
+        //檢查是否為可上傳之副檔名
+        if(in_array($ext,$allow_ext)){
+            //將上傳目的地、剛剛插入的紀錄ID跟附檔名合成成新的檔名
+            $new_file_name=$upload_dir.$_POST['woid'].".".$ext;
+            //搬移檔案至目的地，並確認有沒有完成
+            if(move_uploaded_file($_FILES['wofile']['tmp_name'],$new_file_name)){
+                $sql = "UPDATE `ex_works` SET `woext` = '".$ext."' WHERE `woid` = ".$_POST['woid'];
+                $query = mysqli_query($_con, $sql);
+                header("Location: index.php?result=success");
+                exit;
+            }else{
+                header("Location: index.php?result=unabletoupload");
+                exit;
+            }
+        }else{
+            header("Location: index.php?result=invailedext");
+            exit;
+
+        }
+    }else{
+        header("Location: index.php?result=editsucandnofile");
         exit;
     }
-    //設定結果的初值
-    $result = 0;
-    //判斷運算方式
-    switch($_POST['count']){
-        case "1":
-            $result = $_POST['i']+$_POST['j'];
-            break;
-        case "2":
-            $result = $_POST['i']-$_POST['j'];
-            break;
-        case "3":
-            $result = $_POST['i']*$_POST['j'];
-            break;
-        case "4":
-            $result = $_POST['i']/$_POST['j'];
-            break;
-        default:
-            echo "輸入錯誤";
-            break;
-    }
+}
 
-    //定義運算子顯示符號的鍵值
-    $count_dis = array( "1"=>"+",
-                        "2"=>"-",
-                        "3"=>"*",
-                        "4"=>"/"
-                    );
 
-    //顯示運算過程&結果
-    echo $_POST['i'].$count_dis[$_POST['count']].$_POST['j']."=".$result;
-    //將運算過程與結果傳送回原來的頁面
-    header("Location: index.php?i=".$_POST['i']."&j=".$_POST['j']."&count=".$_POST['count']."&result=".$result);
-    exit;
-*/
+
 ?>
