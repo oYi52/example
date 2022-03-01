7b07a05e3aa677b6d689610b209658f8ddb73690
WenHaoYi <twbbstsuna@gmail.com>
2022-03-01 14:03:47 +0800

    新增文檔

--- process.php
+++ process.php
@@ -1,13 +1,14 @@
 <?php
 
+/* 指定資料庫連線 sql_init */
 $_con = mysqli_connect("localhost","example","kYcM1XuFebgqftpm","example");
+/* .指定資料庫連線 sql_init */
 
-
+/* 作品編輯 work_create */
 if(isset($_POST['action'])&&$_POST['action']=="新增作品"){
     if(isset($_FILES['wofile'])){
         //定義上傳路徑
         echo $upload_dir="uploadfiles/".$_POST['wocategory']."/";
-
         //確認路徑是否存在
         if(!is_dir($upload_dir)){
             //如果不存在，就新增並給予權限
@@ -22,7 +23,6 @@ if(isset($_POST['action'])&&$_POST['action']=="新增作品"){
         $filename=$_FILES['wofile']['name'];
         //透過檔名取得附檔名資訊
         $ext=strtolower(pathinfo($_FILES['wofile']['name'], PATHINFO_EXTENSION));
-        
         //檢查是否為可上傳之副檔名
         if(in_array($ext,$allow_ext)){
             //插入一筆記錄，這裡會先插入一筆記錄是因為等等要用到這筆記錄的ID
@@ -46,45 +46,40 @@ if(isset($_POST['action'])&&$_POST['action']=="新增作品"){
         }else{
             header("Location: index.php?result=invailedext");
             exit;
-
         }
     }else{
         header("Location: index.php?result=nofile");
         exit;
     }
 }
+/* .作品新增 work_create */
 
-
+/* 作品刪除 work_delete */
 if(isset($_POST['action'])&&$_POST['action']=="刪除"){
     echo $sql ="DELETE FROM `ex_works` WHERE `woid` = '".$_POST['woid']."'";
     $query = mysqli_query($_con, $sql);
     header("Location: index.php?result=deleted");
     exit;
 }
+/* .作品刪除 work_delete */
 
-
+/* 作品編輯 work_edit */
 if(isset($_POST['action'])&&$_POST['action']=="編輯作品"){
-
     //先編輯文本
     $sql = "UPDATE `ex_works` SET `wocategory` = '".$_POST['wocategory']."', `wotitle` = '".$_POST['wotitle']."', `woyear` = '".$_POST['woyear']."', `womaterial` = '".$_POST['womaterial']."', `wosize` = '".$_POST['wosize']."', `wostatus` = '".$_POST['wostatus']."' WHERE `woid` = ".$_POST['woid'];
     $query = mysqli_query($_con, $sql);
-
     //判斷是否有改類別
     /* 這次的回家作業寫在這裡 */
     if($_POST['wocategory']!=$_POST['wocatorigin']){
         $origin_dir="uploadfiles/".$_POST['wocatorigin']."/";
         $new_dir="uploadfiles/".$_POST['wocategory']."/";
         $file_name = $_POST['woid'].".".$_POST['woext'];
-
         rename($origin_dir.$file_name,$new_dir.$file_name);
-
     }
-    
     //再判斷是不是有重新上傳檔案
     if(isset($_FILES['wofile'])){
         //定義路徑
         echo $upload_dir="uploadfiles/".$_POST['wocategory']."/";
-
         //確認路徑是否存在
         if(!is_dir($upload_dir)){
             //如果不存在，就新增並給予權限
@@ -99,7 +94,6 @@ if(isset($_POST['action'])&&$_POST['action']=="編輯作品"){
         $filename=$_FILES['wofile']['name'];
         //透過檔名取得附檔名資訊
         $ext=strtolower(pathinfo($_FILES['wofile']['name'], PATHINFO_EXTENSION));
-        
         //檢查是否為可上傳之副檔名
         if(in_array($ext,$allow_ext)){
             //將上傳目的地、剛剛插入的紀錄ID跟附檔名合成成新的檔名
@@ -117,13 +111,13 @@ if(isset($_POST['action'])&&$_POST['action']=="編輯作品"){
         }else{
             header("Location: index.php?result=invailedext");
             exit;
-
         }
     }else{
         header("Location: index.php?result=editsucandnofile");
         exit;
     }
 }
+/* .作品編輯 work_edit */
 
 
 
